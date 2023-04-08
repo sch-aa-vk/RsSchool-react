a57c1236e9d04025a55d36686e53fc66bf2235e4
sch-aa-vk <sch.aa.vk@gmail.com>
2023-04-09 00:32:49 +0600

    feat: add loading icon

--- src/layouts/HomeCards/index.tsx
+++ src/layouts/HomeCards/index.tsx
@@ -4,6 +4,7 @@ import { HomeCard } from '../../components/HomeCard';
 import { ICard } from '../../utils/types';
 
 import './style.css';
+import { LoadingIcon } from '../../assets/LoadingIcon';
 
 interface IHomeCards {
   value: string;
@@ -12,6 +13,7 @@ interface IHomeCards {
 export const HomeCards: React.FC<IHomeCards> = ({ value }: IHomeCards) => {
   const [data, setData] = useState<Array<ICard>>([]);
   const [isPending, setIsPending] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getDataFromApi();
@@ -30,18 +32,39 @@ export const HomeCards: React.FC<IHomeCards> = ({ value }: IHomeCards) => {
     }
   };
 
+  const loadMoreData = async () => {
+    const pageNum = Math.floor(data.length / 25) + 1;
+    if (pageNum < 945) {
+      setIsLoading(true);
+      try {
+        await fetch(` https://api.jikan.moe/v4/anime?sfw&page=${pageNum}`)
+          .then((res) => res.json())
+          .then((res) => (res ? setData([...data, ...res.data]) : setData([])));
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
   return (
     <>
       <div className="cards__wrapper">
-        {data.length > 0 ? (
+        {data.length > 0 && !isPending ? (
           data.map((item) => {
             return <HomeCard key={item.mal_id} item={item} />;
           })
-        ) : (
-          <p>anime not found</p>
-        )}
+        ) : !isPending ? (
+          <p className="cards__wrapper-text">anime not found</p>
+        ) : null}
       </div>
-      {isPending && <p>is loading...</p>}
+      {value.length === 0 && !isLoading && !isPending ? (
+        <button className="cards__button" onClick={loadMoreData}>
+          Load More...
+        </button>
+      ) : null}
+      {(isPending || isLoading) && <LoadingIcon />}
     </>
   );
 };
