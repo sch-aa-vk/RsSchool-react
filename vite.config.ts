88ef63a3f12ee2aafab2e1e3c1d4202d05f18061
sch-aa-vk <sch.aa.vk@gmail.com>
2023-05-27 00:07:57 +0600

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RsSchool-react/',
+  base: 'https://sch-aa-vk.github.io/RsSchool-react/',
   test: {
     globals: true,
     environment: 'jsdom',
